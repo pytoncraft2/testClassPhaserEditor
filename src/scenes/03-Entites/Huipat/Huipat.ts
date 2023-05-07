737c52b750a13267a79a6bf3a48b1fe0f60d0847
pytoncraft2 <pythoncraft2@gmail.com>
2023-05-07 17:57:04 +0200

    🚧 extend base entites

---
+++ src/scenes/03-Entites/Huipat/Huipat.ts
@@ -0,0 +1,29 @@
+
+// You can write more code here
+
+/* START OF COMPILED CODE */
+
+import BaseEntites from "../BaseEntites";
+/* START-USER-IMPORTS */
+/* END-USER-IMPORTS */
+
+export default class Huipat extends BaseEntites {
+
+	constructor(scene: Phaser.Scene, x?: number, y?: number, texture?: string, frame?: number | string) {
+		super(scene, x ?? 536, y ?? 297, texture || "FufuSuperDino", frame);
+
+		/* START-USER-CTR-CODE */
+		// Write your code here.
+		/* END-USER-CTR-CODE */
+	}
+
+	/* START-USER-CODE */
+
+	// Write your code here.
+
+	/* END-USER-CODE */
+}
+
+/* END OF COMPILED CODE */
+
+// You can write more code here
