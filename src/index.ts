d700c84ce6f700f1b421e4c52c6f1f0eb9dbca1c
pytoncraft2 <pythoncraft2@gmail.com>
2023-05-08 18:39:21 +0200

    taille jeu

--- src/index.ts
+++ src/index.ts
@@ -24,12 +24,12 @@ class Boot extends Phaser.Scene {
 window.addEventListener('load', function () {
 	
 	const game = new Phaser.Game({
-		width: 1280,
-		height: 720,
+		width: 1920,
+		height: 1080,
 		backgroundColor: "#2f2f2f",
 		scale: {
-			mode: Phaser.Scale.ScaleModes.FIT,
-			autoCenter: Phaser.Scale.Center.CENTER_BOTH
+			mode: Phaser.Scale.ENVELOP,
+			autoCenter: Phaser.Scale.CENTER_BOTH
 		},
 		physics: {
 			default: 'arcade',
