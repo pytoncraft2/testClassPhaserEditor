0caafb53cc14aaaba01c13ef5245706364f38805
pytoncraft2 <pythoncraft2@gmail.com>
2023-05-27 23:09:57 +0200

    test ajout class groupeToile

---
+++ src/scenes/03-Entites/Huipat/GroupeToile.ts
@@ -0,0 +1,29 @@
+
+// You can write more code here
+
+/* START OF COMPILED CODE */
+
+import Phaser from "phaser";
+/* START-USER-IMPORTS */
+/* END-USER-IMPORTS */
+
+export default class GroupeToile extends Phaser.Physics.Arcade.Group {
+
+	constructor(scene: Phaser.Scene, x?: number, y?: number, texture?: string, frame?: number | string) {
+		super(scene.physics.world, scene);
+
+		/* START-USER-CTR-CODE */
+		// Write your code here.
+		/* END-USER-CTR-CODE */
+	}
+
+	/* START-USER-CODE */
+
+	// Write your code here.
+
+	/* END-USER-CODE */
+}
+
+/* END OF COMPILED CODE */
+
+// You can write more code here
